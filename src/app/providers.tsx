1d8369f093ac57ec79a30eb55920f1b2782e7f67
Ahmed Samy <ahmedsamy999988@gmail.com>
2025-09-08 23:56:59 +0300

    init: setup shadecn all comonents

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,22 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { HeroUIProvider } from "@heroui/react";
 import { ThemeProvider } from "next-themes";
 
+declare module "@react-types/shared" {
+  interface RouterConfig {
+    routerOptions: NonNullable<
+      Parameters<ReturnType<typeof useRouter>["push"]>[1]
+    >;
+  }
+}
+
 export function Providers({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
   return (
-    <HeroUIProvider>
+    <HeroUIProvider navigate={router.push}>
       <ThemeProvider attribute="class" defaultTheme="system">
         {children}
       </ThemeProvider>
