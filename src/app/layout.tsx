41c47be43cecfa2fc32a9a9bc3a042ac2d9261c6
Ahmed Samy <ahmedsamy999988@gmail.com>
2025-09-19 02:41:39 +0300

    feat(inngest): integrate inngest for background job processing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Providers } from './providers';
 import { TRPCReactProvider } from '@/trpc/client';
+import { Toaster } from '@/components/ui/sonner';
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -30,7 +31,10 @@ export default function RootLayout({
 				<body
 					className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 				>
-					<Providers>{children}</Providers>
+					<Providers>
+						<Toaster />
+						{children}
+					</Providers>
 				</body>
 			</html>
 		</TRPCReactProvider>
